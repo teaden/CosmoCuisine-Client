8180f2775ae0f95f52dc914161effa005e2eb6e6
Tyler Eaden <tyray402@yahoo.com>
2024-12-13 12:26:02 -0700

    Initial Commit

---
+++ CosmoCuisine-Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CosmoCuisine-Client
+//
+//  Created by Tyler Eaden on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
